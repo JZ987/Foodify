98483772b6c68b91188a3371ca8800744d265043
Jack Zheng <jzheng7@stuy.edu>
2017-10-15 00:05:52 -0400

    Implement twilio api

--- main.py
+++ main.py
@@ -1,26 +1,77 @@
-import unirest
+import requests
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
 
-def returnRecipes(input):
-    query = ""
+app = Flask(__name__)
 
-    for item in input:
-        query += item + ","
+@app.route("/", methods=['GET', 'POST'])
 
-    query = query.lower().replace(" ","+").replace(",","%2C")
+def incoming_sms():
+    """Send a dynamic reply to an incoming text message"""
+    # Get the message the user sent our Twilio number
+    body = request.values.get('Body', None)
     key = open("apikey.txt").read().replace("\n", "")
-
-    response = unirest.get("https://community-food2fork.p.mashape.com/search?key=" + key + "&q=" + query,
+    if body == None:
+        body = "chicken"
+    response = requests.get("https://community-food2fork.p.mashape.com/search?key=" + key + "&q=" + body,
         headers={
         "X-Mashape-Key": "rjq7HBFHZTmshDawy37VfzQb0HNKp118a2Jjsnp4pTmBWsnAO7",
         "Accept": "application/json"
         }
-    ).body
+    ).json()
+    # Start our TwiML response
+    resp = MessagingResponse()
 
-    recipes = {}
+    # Determine the right reply for this message
+    resp.message(response["recipes"][0]["title"] + " " +  response["recipes"][0]["source_url"])
 
-    for i in range(5):
-        recipes[response["recipes"][i]["title"]] = response["recipes"][i]["source_url"]
+    return str(resp)
 
-#test case 
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# def returnRecipes(input):
+#     query = ""
+#
+#     for item in input:
+#         query += item + ","
+#
+#     query = query.lower().replace(" ","+").replace(",","%2C")
+#     key = open("apikey.txt").read().replace("\n", "")
+#
+#     response = requests.get("https://community-food2fork.p.mashape.com/search?key=" + key + "&q=" + query,
+#         headers={
+#         "X-Mashape-Key": "rjq7HBFHZTmshDawy37VfzQb0HNKp118a2Jjsnp4pTmBWsnAO7",
+#         "Accept": "application/json"
+#         }
+#     ).body
+#
+#     return recipes[response["recipes"][i]["title"]] + " " +  response["recipes"][i]["source_url"]
+
+# def returnRecipes(input):
+#     query = ""
+#
+#     for item in input:
+#         query += item + ","
+#
+#     query = query.lower().replace(" ","+").replace(",","%2C")
+#     key = open("apikey.txt").read().replace("\n", "")
+#
+#     response = requests.get("https://community-food2fork.p.mashape.com/search?key=" + key + "&q=" + query,
+#         headers={
+#         "X-Mashape-Key": "rjq7HBFHZTmshDawy37VfzQb0HNKp118a2Jjsnp4pTmBWsnAO7",
+#         "Accept": "application/json"
+#         }
+#     ).body
+#
+#     return recipes[response["recipes"][i]["title"]] + " " +  response["recipes"][i]["source_url"]
+    # recipes = {}
+    #
+    # for i in range(5):
+    #     recipes[response["recipes"][i]["title"]] = response["recipes"][i]["source_url"]
+    #
+    # return recipes
+
+#test case
 #       for i in range(5):
 #       print recipes.popitem()
