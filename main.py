bed025dc06995c9221a0e31ff91bebd43711004f
Anthony Zeng <anthonyzeng99@gmail.com>
2017-10-15 03:42:09 -0400

    completed most of the user interaction

--- main.py
+++ main.py
@@ -1,31 +1,65 @@
 import requests
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient()
+db = client.test
 
 @app.route("/", methods=['GET', 'POST'])
+
 
 def incoming_sms():
     """Send a dynamic reply to an incoming text message"""
     # Get the message the user sent our Twilio number
-    body = request.values.get('Body', None)
+    user_number = request.values.get('From', None)
+    body = request.values.get('Body', None).lower();
     key = open("apikey.txt").read().replace("\n", "")
-    if body == None:
-        body = "chicken"
-    response = requests.get("https://community-food2fork.p.mashape.com/search?key=" + key + "&q=" + body,
+    ingredients_to_query = ""
+    print(request.values)
+    if (body == 'search'):
+        user = db.users.find_one({'phone_number':user_number})
+        ingredients = user['ingredients']
+        for ingredients in ingredients:
+            ingredients_to_query += ingredients + " "
+
+        db.users.remove({"phone_number" : user_number});
+    else:
+        if (db.users.count({'phone_number':user_number}) == 0):
+            db.users.insert({'phone_number':user_number, 'ingredients':[body]})
+        else:
+            result = db.users.update_one({'phone_number':user_number}, {"$push":{'ingredients':body}})
+
+
+    response = requests.get("https://community-food2fork.p.mashape.com/search?key=" + key + "&q=" + ingredients_to_query,
         headers={
         "X-Mashape-Key": "rjq7HBFHZTmshDawy37VfzQb0HNKp118a2Jjsnp4pTmBWsnAO7",
         "Accept": "application/json"
-        }
-    ).json()
+        }).json()
+
+
     # Start our TwiML response
     resp = MessagingResponse()
 
     # Determine the right reply for this message
     resp.message(response["recipes"][0]["title"] + " " +  response["recipes"][0]["source_url"])
 
+
     return str(resp)
+
+def request_recipe():
+    response = requests.get("https://community-food2fork.p.mashape.com/search?key=" + key + "&q=" + body,
+        headers={
+        "X-Mashape-Key": "rjq7HBFHZTmshDawy37VfzQb0HNKp118a2Jjsnp4pTmBWsnAO7",
+        "Accept": "application/json"
+        }
+    ).json()
+
+
+            
+    
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
@@ -56,7 +90,7 @@ if __name__ == "__main__":
 #
 #     query = query.lower().replace(" ","+").replace(",","%2C")
 #     key = open("apikey.txt").read().replace("\n", "")
-#
+_#
 #     response = requests.get("https://community-food2fork.p.mashape.com/search?key=" + key + "&q=" + query,
 #         headers={
 #         "X-Mashape-Key": "rjq7HBFHZTmshDawy37VfzQb0HNKp118a2Jjsnp4pTmBWsnAO7",
